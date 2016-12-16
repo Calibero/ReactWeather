6991b1777ff9fcbedb48bf17a95d9eb72f8a51f4
Pavel Boček <paja.bocek@gmail.com>
2016-12-16 01:02:48 +0100

    Update about page

--- public/bundle.js
+++ public/bundle.js
@@ -27143,9 +27143,18 @@
 
 	var About = function About(props) {
 	  return React.createElement(
-	    'h3',
+	    'div',
 	    null,
-	    'About Component'
+	    React.createElement(
+	      'h3',
+	      null,
+	      'About'
+	    ),
+	    React.createElement(
+	      'p',
+	      null,
+	      'Welcome to the about page.'
+	    )
 	  );
 	};
 
@@ -27161,9 +27170,18 @@
 
 	var Examples = function Examples(props) {
 	  return React.createElement(
-	    'h3',
+	    'div',
 	    null,
-	    'Examples Component'
+	    React.createElement(
+	      'h3',
+	      null,
+	      'Examples'
+	    ),
+	    React.createElement(
+	      'p',
+	      null,
+	      'Welcome to examples page!'
+	    )
 	  );
 	};
 
